c98c008374d72c80f7827cc0fbc1e64bc6944b9d
Kaak99 <cours.kaak@gmail.com>
2019-01-08 22:18:46 +0400

    string.xml fr, alertdialog xml

--- app/src/main/java/com/example/souhi/moodtracker/MainActivity.java
+++ app/src/main/java/com/example/souhi/moodtracker/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.souhi.moodtracker;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+
 import com.google.gson.Gson;
 
 
@@ -31,6 +37,15 @@ public class MainActivity extends AppCompatActivity {
         btnNoteAdd = findViewById(R.id.btnNoteAdd);
         btnHistory = findViewById(R.id.btnHistory);
         mainLayout = findViewById(R.id.mainLayout);
+
+
+// if  swiping
+
+// if clicking button noteAdd
+
+// if clicking button History
+
+
 
 
 
