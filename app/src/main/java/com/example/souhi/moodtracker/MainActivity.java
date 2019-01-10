283613294b9f2a1c173c45c9e1b40ed2e071bb6d
Kaak99 <cours.kaak@gmail.com>
2019-01-10 06:23:41 +0400

    portrait only in manifest . Add onpause&onresume in main

--- app/src/main/java/com/example/souhi/moodtracker/MainActivity.java
+++ app/src/main/java/com/example/souhi/moodtracker/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.media.MediaPlayer;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -13,6 +14,8 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import com.google.gson.Gson;
+
+import java.util.Calendar;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -110,6 +113,72 @@ public class MainActivity extends AppCompatActivity {
 
 
     } //end onCreate
+
+//onPause : save actual mood object (via json/gson) in SharedPref key ["memory"+index] (index 0-8)
+// first need to know if same day than last key shared. if different : index+1
+    @Override
+    protected void onPause() {
+        super.onPause();
+        index = prefs.getInt("memoryIndex", 1);
+        String json0 = prefs.getString("memory" + index, "");
+
+        if (json0 != null && !json0.equals("")) {
+            lastMood = gson.fromJson(json0, Mood.class);
+            Calendar calendar1 = Calendar.getInstance();
+            Calendar calendar2 = Calendar.getInstance();
+            calendar1.setTimeInMillis(System.currentTimeMillis());//today's date
+            calendar2.setTimeInMillis(lastMood.getTodaysDate());//date from lastMood
+
+            if (calendar1.get(Calendar.DAY_OF_YEAR) != calendar2.get(Calendar.DAY_OF_YEAR) || calendar1.get(Calendar.YEAR) != calendar2.get(Calendar.YEAR)) {
+                index++;// different date-> index+1
+                if (index == 9)
+                    index = 1; // index= 1 to 8
+            }
+        }
+//now we save the mood at SharedPref key ["memory" +index]
+        currentMood.setTodaysDate(System.currentTimeMillis()); //actualize date before saving
+        String json = gson.toJson(currentMood); // currentMood -> json format
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString("memory" + index, json);// writing json in key ["memory" + index]
+        editor.putInt("memoryIndex", index);//writing last index in key memoryIndex
+        editor.apply();
+    }  // end on pause
+
+//onResume : restore lastmode registered if date = todays date (else, reinit smiley/background)
+    @Override
+    protected void onResume() {
+        super.onResume();
+        SharedPreferences prefs;
+        prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        index = prefs.getInt("memoryIndex", 1);
+        String json = prefs.getString("memory" + index, "");//json contains lastmood registered
+        if (json != null && !json.equals("")) {
+            currentMood = gson.fromJson(json, Mood.class);//currentMood contains lastmood registered
+            Calendar calendar1 = Calendar.getInstance();
+            Calendar calendar2 = Calendar.getInstance();
+            calendar1.setTimeInMillis(System.currentTimeMillis());//date du jour
+            calendar2.setTimeInMillis(currentMood.getTodaysDate());//date de currentMood
+            if (calendar1.get(Calendar.DAY_OF_YEAR) != calendar2.get(Calendar.DAY_OF_YEAR) || calendar1.get(Calendar.YEAR) != calendar2.get(Calendar.YEAR)) {
+// if currentMood(=last moood registerde) is not from today, then reinit the mood (smiley3, green background, no comment, date=now)
+                moodNumber = 3;
+                moodComment = "";
+                long moodDate = System.currentTimeMillis();
+                currentMood.setTodaysMood(moodNumber);//
+                currentMood.setTodaysNote(moodComment);
+                currentMood.setTodaysDate(moodDate);
+            } else { // if this last mood is from today, then restore
+                moodNumber = currentMood.getTodaysMood();
+                moodComment = currentMood.getTodaysNote();
+                moodDate = currentMood.getTodaysDate();
+            }
+// Now : display the right smiley and background color
+            ivSmiley.setImageResource(Constants.tabSmiley[moodNumber]);
+            mainLayout.setBackgroundResource(Constants.tabColorBackground[moodNumber]);
+        }
+    } //end onCreate
+
+
+
 
     private void swipeDisplay() {  //actualize display and sound when swipping
         ivSmiley.setImageResource(Constants.tabSmiley[moodNumber]);
